412dcf58456daddc74299ab6d7931b479c82ddc4
harsha1636 <harsha60201@gmail.com>
2025-06-16 21:54:04 +0530

    UserRepository Used for communicating with Database

--- src/main/java/com/example/Bankify/repository/UserRepository.java
+++ src/main/java/com/example/Bankify/repository/UserRepository.java
@@ -1,5 +1,8 @@
 package com.example.Bankify.repository;
 
-public interface UserRepository {
+import com.example.Bankify.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
