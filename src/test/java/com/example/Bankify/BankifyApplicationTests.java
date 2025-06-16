8e5ced810e8c5332b312908b7ccc45b0fe35bd50
harsha1636 <harsha60201@gmail.com>
2025-06-16 16:24:57 +0530

    Initial commit - Bankify Spring Boot project

---
+++ src/test/java/com/example/Bankify/BankifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Bankify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
